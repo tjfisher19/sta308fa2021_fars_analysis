a2aa69e32d5532b17d5a1214d738cb11ef5a3b9f
Thomas Fisher <40180281+tjfisher19@users.noreply.github.com>
2021-09-25 13:50:55 -0400

    Add files via upload

---
+++ sta308_week06_day15_starter.R
@@ -0,0 +1,73 @@
+######################################
+## 
+##  sta308_week06_farsAPI_analysis.R
+##
+##  Starter code provided by Bailer/Fisher
+##
+##  This starter file will be updated over the 
+##   coming classes with more content, but for
+##   now it does the following:
+##
+## 1. demonstrates functionality of the fromJSON()
+##    function and demonstrates the NHTSA FARS API
+## 2. Provides a function that will fetch the crash
+##    data for a given county in Ohio (specified by
+##    a FIPS code number: 1, 3, 5,..., 173, 175
+##    see: https://en.wikipedia.org/wiki/List_of_counties_in_Ohio
+##
+## Task: Your task is to take this output from the API
+##       and build a data.frame where each row
+##       corresponds to a County in Ohio and the
+##       recorded number of fatalities in 2019.
+
+library(jsonlite)
+
+################################
+## First, we look at the output 
+##   from the fromJSON() function
+## Here, we use the sample API call
+##   provided on https://crashviewer.nhtsa.dot.gov/CrashAPI
+##   for summary counts.
+fromJSON("https://crashviewer.nhtsa.dot.gov/CrashAPI/analytics/GetInjurySeverityCounts?fromCaseYear=2014&toCaseYear=2015&state=1&format=json")
+
+###################
+## Note that the output is a named list
+##   It is quite easy to extract information
+##   from the output of the fromJSON() function.
+##
+
+###########################
+## Now we learn how to get the data of interest
+##   Here we collect for Ohio County 17 - Butler County
+##   for the year 2014
+butler_co <- fromJSON("https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCrashesByLocation?fromCaseYear=2014&toCaseYear=2014&state=39&county=17&format=json")
+butler_co
+## By looking at this single county we can start
+##   to understand what information we will need later
+## The variable FATALS in Results includes the number 
+##   of fatalities for each crash.
+## Our ultimate goal is to aggregate all those results
+##   for each county in 2019
+
+#################################
+## get_county_json(county=1)
+##
+## This function takes the provided 'county' number
+##   parameter and appends it to the website/API
+##   call. The result of the API call is then output.
+## We use this function so we can lapply() over
+##   all valid county FIPS codes.
+get_county_json <- function(county=1) {
+  site <- paste0("https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCrashesByLocation?fromCaseYear=2019&toCaseYear=2019&state=39&county=", 
+                 county, 
+                 "&format=json")
+  fromJSON(site)
+}
+
+all_county_list <- lapply(seq(1,175,2), get_county_json)
+
+##################################
+## In the below lines, write code
+##   that extracts the county name &
+##   number of fatalities in that county
+
